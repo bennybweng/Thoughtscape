a1e7c3367530159233927bf3f5fa59c62d3478ef
Benjamin Weng <benjaminweng@gmx.de>
2023-04-12 21:55:57 +0200

    pie chart

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -11,7 +11,6 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
   List<String> moods = [
     "neutral",
     "angry",
@@ -25,32 +24,205 @@ class _ProfilePageState extends State<ProfilePage> {
     "sad4"
   ];
 
+  double radius = 100;
+
+  int touchedIndex = -1;
+
+  final shadows = [const Shadow(color: Colors.black, blurRadius: 2)];
+
+  int days = 7;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-          child: PieChart(PieChartData(sections: [
-        for(String mood in moods)
-          if (countMoods()[moods.indexOf(mood)] != 0)
-            PieChartSectionData(value: countMoods()[moods.indexOf(mood)].toDouble(), title: mood)
-
-      ]))),
+      body: ListView(
+        children: [
+          diagramOne(context)
+        ],
+      ),
+    );
+  }
+  
+  Widget diagramOne(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 300,
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondaryContainer,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: PieChart(PieChartData(
+                  pieTouchData: PieTouchData(
+                    touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                      setState(() {
+                        if (!event.isInterestedForInteractions ||
+                            pieTouchResponse == null ||
+                            pieTouchResponse.touchedSection == null) {
+                          touchedIndex = -1;
+                          return;
+                        }
+                        touchedIndex = pieTouchResponse
+                            .touchedSection!.touchedSectionIndex;
+                      });
+                    },
+                  ),
+                  sections: [
+                    for (String mood in existingMoods(days))
+                      PieChartSectionData(
+                          badgePositionPercentageOffset: .98,
+                          badgeWidget: AnimatedContainer(
+                              width: touchedIndex ==
+                                  existingMoods(days).indexOf(mood)
+                                  ? 50 + 20
+                                  : 50,
+                              height: touchedIndex ==
+                                  existingMoods(days).indexOf(mood)
+                                  ? 50 + 20
+                                  : 50,
+                              duration: PieChart.defaultDuration,
+                              child: Mood(mood).toImage()),
+                          radius:
+                          touchedIndex == existingMoods(days).indexOf(mood)
+                              ? radius + 10
+                              : radius,
+                          value: countMoods(days)[moods.indexOf(mood)].toDouble(),
+                          titleStyle: TextStyle(
+                            fontSize:
+                            touchedIndex == existingMoods(days).indexOf(mood)
+                                ? 20 + 5
+                                : 20,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xffffffff),
+                            shadows: shadows,
+                          ),
+                          title:
+                          "${((countMoods(days)[moods.indexOf(mood)] / sumMoods(days)) * 100).round()}%",
+                          color: Mood(mood).toColor())
+                  ],
+                  sectionsSpace: 0,
+                  centerSpaceRadius: 0)),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Material(
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                    elevation: 0.1,
+                    child: PopupMenuButton(
+                        position: PopupMenuPosition.under,
+                        initialValue: days,
+                        onSelected: (int value) {
+                          setState(() {
+                            days = value;
+                          });
+                        },
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              days > 0 ? "Letzte $days Tage" : "Alle",
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSecondaryContainer),
+                            ),
+                            Icon(Icons.arrow_drop_down)
+                          ],
+                        ),
+                        itemBuilder: (BuildContext context) =>
+                        <PopupMenuEntry<int>>[
+                          const PopupMenuItem(
+                            value: 7,
+                            child: Text("Letzte 7 Tage"),
+                          ),
+                          const PopupMenuDivider(
+                            height: 0,
+                          ),
+                          const PopupMenuItem(
+                            value: 30,
+                            child: Text("Letzte 30 Tage"),
+                          ),
+                          const PopupMenuDivider(
+                            height: 0,
+                          ),
+                          const PopupMenuItem(
+                            value: 90,
+                            child: Text("Letzte 90 Tage"),
+                          ),
+                          const PopupMenuDivider(
+                            height: 0,
+                          ),
+                          const PopupMenuItem(
+                            value: -1,
+                            child: Text("Alle"),
+                          ),
+                        ]),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
-  List<int> countMoods() {
-
+  List<int> countMoods(int days) {
     List<int> count = List.generate(moods.length, (index) => 0);
-    List<Entry> entries = SharedPrefs().getEntries();
-    for (Entry entry in entries) {
+    List<Entry> entries = [];
+    if (days > 0) {
+      for (Entry entry in SharedPrefs().getEntries()) {
+        if (isBetween(DateTime.now().subtract(Duration(days: days)), DateTime.now(), entry.date)) {
+          entries.add(entry);
+        }
+      }
+    }else {
+      entries = SharedPrefs().getEntries();
+    }
+    /*for (Entry entry in entries) {
       for (String mood in moods) {
         if (mood == entry.mood.info) {
           count[moods.indexOf(mood)] += 1;
           continue;
         }
       }
+    }*/
+    for (Entry entry in entries) {
+          count[moods.indexOf(entry.mood.info)] += 1;
     }
     return count;
+  }
+
+  bool isBetween(DateTime fromDateTime, DateTime toDateTime, DateTime toCheck) {
+    return (toCheck.isAfter(fromDateTime) ||
+        toCheck.isAtSameMomentAs(fromDateTime)) &&
+        (toCheck.isBefore(toDateTime) ||
+        toCheck.isAtSameMomentAs(toDateTime));
+  }
+
+  int sumMoods(int days) {
+    return countMoods(days)
+        .fold(0, (previousValue, element) => previousValue + element);
+  }
+
+  List<String> existingMoods(int days) {
+    List<String> existingMoods = [];
+    for (String mood in moods) {
+      if (countMoods(days)[moods.indexOf(mood)] != 0) {
+        existingMoods.add(mood);
+      }
+    }
+    return existingMoods;
   }
 }
--- lib/types/entry.dart
+++ lib/types/entry.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../shared/shared_prefs.dart';
 class Entry {
   DateTime date;
   String title;
@@ -43,6 +45,19 @@ class Entry {
 
 }
 
+const Color m3BaseColor = Color(0xff6750a4);
+const List<Color> colorOptions = [
+  m3BaseColor,
+  Colors.indigo,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.pink
+];
+
 class Mood{
 
   String info;
@@ -70,6 +85,83 @@ class Mood{
       default:
         return Image.asset("assets/neutral.png");
     }
+
+
   }
+
+  Color toColor(){
+    Color base = colorOptions[SharedPrefs().getColor()];
+    switch(info) {
+      case "neutral":
+        return base;
+      case "angry":
+        return base.withOpacity(0.9);
+      case "love":
+        return base.withOpacity(0.8);
+      case "happy1":
+        return base.withOpacity(0.7);
+      case "happy2":
+        return base.withOpacity(0.6);
+      case "sad2":
+        return base.withOpacity(0.5);
+      case "sad3":
+        return base.withOpacity(0.4);
+      case "sad4":
+        return base.withOpacity(0.3);
+      default:
+        return base;
+    }
+  }
+
+
+  Color toColor2(){
+    switch(info) {
+      case "neutral":
+        return Color(0xff6750a4);
+      case "angry":
+        return Colors.deepOrange;
+      case "love":
+        return Colors.pink;
+      case "happy1":
+        return Colors.green;
+      case "happy2":
+        return Colors.orange;
+      case "sad2":
+        return Colors.teal;
+      case "sad3":
+        return Colors.blue;
+      case "sad4":
+        return Colors.indigo;
+      default:
+        return Color(0xFFFC952D);
+    }
+  }
+
+  Color toColor3(){
+    switch(info) {
+      case "neutral":
+        return Color(0xff6750a4);
+      case "angry":
+        return Color(0xe56750a4);
+      case "love":
+        return Color(0xcc6750a4);
+      case "happy1":
+        return Color(0xb36750a4);
+      case "happy2":
+        return Color(0x996750a4);
+      case "sad2":
+        return Color(0x806750a4);
+      case "sad3":
+        return Color(0x666750a4);
+      case "sad4":
+        return Color(0x4d6750a4);
+      default:
+        return Color(0xff6750a4);
+    }
+  }
+
+
+
+
 
 }
